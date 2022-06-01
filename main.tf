63005ad67b31ac27dc602b2d9bfcfa05967e1c1b
aacheson1 <106710084+aacheson1@users.noreply.github.com>
2022-06-01 16:12:53 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,4 +15,11 @@ module "server" {
   public_key   = var.public_key
   private_key  = var.private_key
 }
-
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.72.0"
+    }
+  }
+}
